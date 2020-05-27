93a2563e96740560a0d7213c330f3fc0a0b5a382
Matthew <matthew.young@reading.ac.uk>
2020-05-27 19:37:43 +0100

    loop through fcst months

--- average_sforecast_probabilities_region.py
+++ average_sforecast_probabilities_region.py
@@ -55,49 +55,51 @@ p_name = 'tercile' # give categories an appropriate name
 
 model=0
 region='Kenya'
-month_start=12
-month_end=12
+month_start=[10,11,12]
+month_end=[12,12,12]
 month_lead=0
 
-p_file = dir_in+center_ids[model]+'_total_rainfall_'+p_name+'_probabilities_'+region+'_s'+mon_string(month_start)+'_e'+mon_string(month_end)+'_lead'+str(month_lead)+'.nc'
-nc_fid = Dataset(p_file, 'r')
-c3s_lat = np.flip(np.array(nc_fid.variables['latitude'][:]))  # extract/copy the data
-c3s_lon = np.array(nc_fid.variables['longitude'][:])
-p_all = np.array(nc_fid.variables['p'][:]) #  probabilities
-nc_fid.close()
+for m in np.arange(0,len(month_start)):
+  print 'averaging fcst start-month '+str(month_start[m])+' & end-month '+str(month_end[m])
+  p_file = dir_in+center_ids[model]+'_total_rainfall_'+p_name+'_probabilities_'+region+'_s'+mon_string(month_start[m])+'_e'+mon_string(month_end[m])+'_lead'+str(month_lead)+'.nc'
+  nc_fid = Dataset(p_file, 'r')
+  c3s_lat = np.flip(np.array(nc_fid.variables['latitude'][:]))  # extract/copy the data
+  c3s_lon = np.array(nc_fid.variables['longitude'][:])
+  p_all = np.array(nc_fid.variables['p'][:]) #  probabilities
+  nc_fid.close()
 
-mask_shape = gpd.read_file(shapefile)
-[lon2d,lat2d] = np.meshgrid(c3s_lon,c3s_lat)
-# lon2 = lon2d.reshape(-1)
-# lat2 = lat2d.reshape(-1)
-lon2=lon2d.flatten()
-lat2=lat2d.flatten()
-# Create empty mask
-mask = []
-# Fill mask with true / false
-for lat, lon in zip(lat2, lon2):
-  this_point = shapely.geometry.Point(lon, lat)
-  res = mask_shape.contains(this_point)
-  mask.append(res.values[0])
+  mask_shape = gpd.read_file(shapefile)
+  [lon2d,lat2d] = np.meshgrid(c3s_lon,c3s_lat)
+  # lon2 = lon2d.reshape(-1)
+  # lat2 = lat2d.reshape(-1)
+  lon2=lon2d.flatten()
+  lat2=lat2d.flatten()
+  # Create empty mask
+  mask = []
+  # Fill mask with true / false
+  for lat, lon in zip(lat2, lon2):
+    this_point = shapely.geometry.Point(lon, lat)
+    res = mask_shape.contains(this_point)
+    mask.append(res.values[0])
 
-# Reshape mask
-mask = np.array(mask).reshape(lon2d.shape)
+  # Reshape mask
+  mask = np.array(mask).reshape(lon2d.shape)
 
-# p_mask_test = np.ma.masked_array(p_all[0,0,:,:],mask=mask)
-mask_final = np.copy(mask)*1.0
-mask_final[mask_final==0]=np.nan
-# mask_final[mask_final==True]=1
-p_masked=p_all*mask_final
+  # p_mask_test = np.ma.masked_array(p_all[0,0,:,:],mask=mask)
+  mask_final = np.copy(mask)*1.0
+  mask_final[mask_final==0]=np.nan
+  # mask_final[mask_final==True]=1
+  p_masked=p_all*mask_final
 
-p_region = np.nanmean(p_masked,axis=(2,3))
+  p_region = np.nanmean(p_masked,axis=(2,3))
 
-# save as text file
-f_out = dir_in+center_ids[model]+'_total_rainfall_'+p_name+'_probabilities_avg_'+region+'_s'+mon_string(month_start)+'_e'+mon_string(month_end)+'_lead'+str(month_lead)+'.txt'
-headers=' '.join(tercile_short_name)
-headers='Year '+headers
-fm_list = ['%i','%10.5f','%10.5f','%10.5f']
-np.savetxt(f_out,np.hstack((np.expand_dims(years,axis=1),p_region)),fmt=fm_list,delimiter=' ',header=headers)
-
+  # save as text file
+  f_out = dir_in+center_ids[model]+'_total_rainfall_'+p_name+'_probabilities_avg_'+region+'_s'+mon_string(month_start[m])+'_e'+mon_string(month_end[m])+'_lead'+str(month_lead)+'.txt'
+  headers=' '.join(tercile_short_name)
+  headers='Year '+headers
+  fm_list = ['%i','%10.5f','%10.5f','%10.5f']
+  np.savetxt(f_out,np.hstack((np.expand_dims(years,axis=1),p_region)),fmt=fm_list,delimiter=' ',header=headers)
+  print 'saved'
 #
 # plt.subplot(1,2,1)
 # mymap = Basemap(projection='cyl',resolution='l',\
